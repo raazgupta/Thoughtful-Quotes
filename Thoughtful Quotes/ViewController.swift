50b95f919c90978c1d34bfea8666ff75e21eae7e
Raj Gupta <raazgupta@gmail.com>
2019-01-06 08:23:01 +0900

    Initial Commit

---
+++ Thoughtful Quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thoughtful Quotes
+//
+//  Created by Raj Gupta on 6/1/19.
+//  Copyright © 2019 SoulfulMachine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
